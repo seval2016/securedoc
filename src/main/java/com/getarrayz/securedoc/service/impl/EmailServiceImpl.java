e83e7d99ded951eaa5043f99d81b5af7075cd8cc
seval2016 <sevalsenturk2016@gmail.com>
2024-04-22 15:50:50 +0300

    Email Service

---
+++ src/main/java/com/getarrayz/securedoc/service/impl/EmailServiceImpl.java
@@ -0,0 +1,61 @@
+package com.getarrayz.securedoc.service.impl;
+
+import com.getarrayz.securedoc.exception.ApiException;
+import com.getarrayz.securedoc.service.EmailService;
+import lombok.RequiredArgsConstructor;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class EmailServiceImpl implements EmailService {
+
+
+    private final JavaMailSender sender;
+    private static final String NEW_USER_ACCOUNT_VERIFICATION = "New User Account Verification";
+    private static final String PASSWORD_RESET_REQUEST = "Reset Password Request";
+    @Value("${spring.mail.verify.host}")
+    private String host;
+    @Value("${spring.mail.username}")
+    private String fromEmail;
+
+    @Override
+    @Async
+    public void sendNewAccountEmail(String name, String email, String token) {
+        try{
+            var message=new SimpleMailMessage();
+            message.setSubject(NEW_USER_ACCOUNT_VERIFICATION);
+            message.setFrom(fromEmail);
+            message.setTo(email);
+            message.setText(getEmailMessage(name,host,token));
+            sender.send(message);
+        }catch (Exception exception){
+            log.error(exception.getMessage());
+            throw new ApiException("Unable to send email");
+        }
+    }
+
+    @Override
+    @Async
+    public void sendPasswordResetEmail(String name, String email, String token) {
+
+        try{
+            var message=new SimpleMailMessage();
+            message.setSubject(PASSWORD_RESET_REQUEST);
+            message.setFrom(fromEmail);
+            message.setTo(email);
+            message.setText(getResetPasswordMessage(name,host,token));
+            sender.send(message);
+        }catch (Exception exception){
+            log.error(exception.getMessage());
+            throw new ApiException("Unable to send email");
+        }
+
+    }
+}
